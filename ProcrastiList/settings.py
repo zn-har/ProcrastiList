f71e073493d64bfddbdede687cab4dc036e9e741
zn-har <zenharmuhammed00@gmail.com>
2025-08-09 02:58:22 +0530

    Update ALLOWED_HOSTS to

--- ProcrastiList/settings.py
+++ ProcrastiList/settings.py
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+CSRF_TRUSTED_ORIGINS = ["https://procrastilist.onrender.com", "http://procrastilist.onrender.com"]
+
