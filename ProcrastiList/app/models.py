9742e59cf1179728475524f9e019d9b29e9c3d61
Muhammed Answar C K <answarc764@gmail.com>
2025-08-08 20:33:10 +0530

    Minor fixes.

--- ProcrastiList/app/models.py
+++ ProcrastiList/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Todo(models):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    task = models.CharField(max_length=100, blank=False, null=False)
+    completed = models.BooleanField(default=False)
+    
--- ProcrastiList/app/views.py
+++ ProcrastiList/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 def index(request):
     return render(request, 'index.html')
 
-# Create your views here.
